f819ec7789c62b0b3ab30ad470287c871907b7a6
TomonoriSoejima <tomo@elastic.co>
2020-06-17 10:12:17 +0900

    small change

--- get_time.sh
+++ get_time.sh
@@ -1,6 +1,7 @@
-for time_millis in $(cat result.json | jq '.aggregations.host_name.buckets[] | .key, .taken_time.value' );do
+hostname_identifier=ksc
+for time_millis in $(cat result.json | jq -r '.aggregations.host_name.buckets[] | .key, .taken_time.value' );do
 	
-	if [[ $time_millis != *"ksc"* ]]; then
+	if [[ $time_millis != *${hostname_identifier}* ]]; then
  		 echo "scale=2;${time_millis}/1000/3600" | bc
 	else
 		echo $time_millis
--- push.data.sh
+++ push.data.sh
@@ -1,3 +1,3 @@
 data="bulk.json"
 cat recovery.json | jq -c '.[].shards[] | {index: {_index: "recovery"}}, {source_name: .source.name, target_name: .target.name, taken: .total_time_in_millis }' > $data
-http -a elastic:changeme localhost:9200/_bulk < $data
+http localhost:9200/_bulk < $data
