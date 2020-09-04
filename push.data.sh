8e78facb57c1f3bb75a2bf54441c2ea4ffa1b1a2
TomonoriSoejima <tomo@elastic.co>
2020-09-04 19:13:46 +0900

    doing tonumber for time value

--- push.data.sh
+++ push.data.sh
@@ -1,38 +1,4 @@
-
-url="localhost:9200/recovery"
-curl -s --location --request PUT $url \
---header 'Content-Type: application/json' \
---data-raw '{
-    "mappings" : {
-      "properties" : {
-        "source_node" : {
-          "type" : "text",
-          "fields" : {
-            "keyword" : {
-              "type" : "keyword",
-              "ignore_above" : 256
-            }
-          }
-        },
-        "taken" : {
-          "type" : "long"
-        },
-        "target_node" : {
-          "type" : "text",
-          "fields" : {
-            "keyword" : {
-              "type" : "keyword",
-              "ignore_above" : 256
-            }
-          }
-        }
-      }
-    }
-} '
-
-
-
 data="bulk.json"
 recovery="osk_recovery_20200903.txt"
-cat $recovery | jq -c '.[] | {index: {_index: "recovery"}}, {source_node: .source_node, target_node: .target_node, taken: .time }' > $data
+cat $recovery | jq -c '.[] | {index: {_index: "recovery"}}, {source_node: .source_node, target_node: .target_node, taken:  (.time | tonumber) }' > $data
 http localhost:9200/_bulk < $data
